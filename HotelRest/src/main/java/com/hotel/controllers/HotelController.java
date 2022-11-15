32450f79b70295df33e86d2d61df74c3e4fac7ec
Adam S <adam.said@etu.umontpellier.fr>
2022-11-15 18:11:53 +0100

    [AS] Ajout base projet rest

--- HotelRest/src/main/java/com/hotel/controllers/HotelController.java
+++ HotelRest/src/main/java/com/hotel/controllers/HotelController.java
@@ -3,33 +3,50 @@ package com.hotel.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.hotel.exceptions.HotelNotFoundException;
 import com.hotel.models.Hotel;
 import com.hotel.repositories.HotelRepository;
 
+@RestController
 public class HotelController {
 	
 	@Autowired
 	private HotelRepository repository;
+	private static final String uri = "http://localhost:8080/hotelservice/api";
 	
+	@GetMapping(uri + "/hotels")
 	public List<Hotel> getAllHotels() {
 		return repository.findAll();
 	}
 	
+	@GetMapping(uri + "/hotels/count")
 	public String count() {
 		return String.format("{\"%s\": %d}", "count", repository.count());
 	}
 	
-	public Hotel getHotelById(long id) throws HotelNotFoundException {
-		return repository.findById(id).orElseThrow(() -> new HotelNotFoundException("Error ; could not found hotel by ID"));
+	@GetMapping(uri + "/hotels/{id}")
+	public Hotel getHotelById(@PathVariable long id) throws HotelNotFoundException {
+		return repository.findById(id).orElseThrow(() -> new HotelNotFoundException("Error : could not found hotel by ID"));
 	}
 	
-	public Hotel createHotel(Hotel hotel) {
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(uri + "/hotels")
+	public Hotel createHotel(@RequestBody Hotel hotel) {
 		return repository.save(hotel);
 	}
 	
-	public Hotel upodateHotel(Hotel newHotel, long id) {
+	@GetMapping(uri + "/hotels/{id}")
+	public Hotel updateHotel(@RequestBody Hotel newHotel, @PathVariable long id) {
 		return repository.findById(id).map(hotel -> {
 			hotel.setName(newHotel.getName());
 			hotel.setStars(newHotel.getStars());
@@ -45,7 +62,11 @@ public class HotelController {
 		});
 	}
 	
-	public void deleteEmployee(long id) {
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping(uri + "/hotels/{id}")
+	public void deleteEmployee(@PathVariable long id) throws HotelNotFoundException{
+		Hotel hotel = repository.findById(id).orElseThrow(() -> new HotelNotFoundException("Error : could not found hotel by ID"));
+		repository.delete(hotel);
 		
 	}
 }
--- HotelRest/src/main/java/com/hotel/main/HotelRestApplication.java
+++ HotelRest/src/main/java/com/hotel/main/HotelRestApplication.java
@@ -2,8 +2,20 @@ package com.hotel.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = {
+		"com.hotel.models"
+})
+@EnableJpaRepositories(basePackages = {
+	"com.hotel.repositories"
+})
+@SpringBootApplication(scanBasePackages = {
+		"com.hotel.data",
+		"com.hotel.exceptions",
+		"com.hotel.controllers"
+})
 public class HotelRestApplication {
 
 	public static void main(String[] args) {
