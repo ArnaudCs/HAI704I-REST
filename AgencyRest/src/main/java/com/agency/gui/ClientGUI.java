201247b2099d0a0bfe8af48605b3a05146cd1319
Arnaud <arnaud.cossu@gmail.com>
2022-11-28 16:27:37 +0100

    Ajout de fichiers pour la GUI

--- AgencyRest/src/main/java/com/agency/gui/ClientGUI.java
+++ AgencyRest/src/main/java/com/agency/gui/ClientGUI.java
@@ -17,11 +17,21 @@ import javax.swing.SwingConstants;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import java.awt.Label;
+import java.util.Calendar;
+
+import javax.swing.JMenuBar;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class ClientGUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField destinationInput;
+	private JTextField personeNumberInput;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -52,7 +62,7 @@ public class ClientGUI extends JFrame {
 		raisedbevel = BorderFactory.createRaisedBevelBorder();
 		loweredbevel = BorderFactory.createLoweredBevelBorder();
 		empty = BorderFactory.createEmptyBorder();
-		setTitle("TripAdvisor - Comparateur");
+		setTitle("HotelAdvisor - Comparateur");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1143, 743);
 		contentPane = new JPanel();
@@ -66,7 +76,7 @@ public class ClientGUI extends JFrame {
 		lblComparateur.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComparateur.setForeground(new Color(255, 255, 255));
 		lblComparateur.setFont(new Font("Gayathri Thin", Font.BOLD, 40));
-		lblComparateur.setBounds(413, 15, 303, 48);
+		lblComparateur.setBounds(412, 26, 303, 48);
 		contentPane.add(lblComparateur);
 		
 		JSeparator separator = new JSeparator();
@@ -84,15 +94,62 @@ public class ClientGUI extends JFrame {
 		contentPane.add(destinationInput);
 		destinationInput.setColumns(10);
 		destinationInput.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-
+		destinationInput.setOpaque(false);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("/Users/macbook/Desktop/HAI704I-REST/Media/SearchBar.png"));
+		lblNewLabel.setIcon(new ImageIcon("/home/e20190000683/Bureau/HAI704I-REST/Media/SearchBar.png"));
 		lblNewLabel.setBounds(380, 153, 380, 57);
 		contentPane.add(lblNewLabel);
 		
+		personeNumberInput = new JTextField();
+		personeNumberInput.setBounds(430, 235, 165, 36);
+		contentPane.add(personeNumberInput);
+		personeNumberInput.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		personeNumberInput.setOpaque(false);
+
+
+		personeNumberInput.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(424, 349, 199, 28);
+		contentPane.add(textField_2);
+		
+		JComboBox starsSelector = new JComboBox();
+		starsSelector.setModel(new DefaultComboBoxModel(new String[] {"1 étoile (★)", "2 étoiles (★★)", "3 étoiles (★★★)", "4 étoiles (★★★★)", "5 étoiles (★★★★★)"}));
+		starsSelector.setBounds(452, 299, 171, 24);
+		contentPane.add(starsSelector);
+		
+		JButton searchButton = new JButton("");
+		searchButton.setForeground(new Color(255, 255, 255));
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				destinationInput.setText("test");
+			}
+		});
+		
+		JLabel input = new JLabel("");
+		input.setIcon(new ImageIcon("/home/e20190000683/Bureau/HAI704I-REST/Media/input.png"));
+		input.setBounds(412, 231, 200, 40);
+		contentPane.add(input);
+		
+		JLabel lblIndiquezUneDestination = new JLabel("Indiquez une destination ...");
+		lblIndiquezUneDestination.setForeground(new Color(255, 255, 255));
+		lblIndiquezUneDestination.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIndiquezUneDestination.setBounds(452, 121, 240, 42);
+		contentPane.add(lblIndiquezUneDestination);
+		searchButton.setContentAreaFilled(false);
+		searchButton.setBorderPainted(false);
+		searchButton.setBounds(490, 518, 148, 42);
+		contentPane.add(searchButton);
+		
+		JLabel searchButtonBackground = new JLabel("");
+		searchButtonBackground.setIcon(new ImageIcon("/home/e20190000683/Bureau/HAI704I-REST/Media/valider.png"));
+		searchButtonBackground.setBounds(475, 520, 180, 40);
+		contentPane.add(searchButtonBackground);
+		
 		JLabel background = new JLabel("");
-		background.setIcon(new ImageIcon("/Users/macbook/Desktop/HAI704I-REST/Media/background.jpg"));
+		background.setIcon(new ImageIcon("/home/e20190000683/Bureau/HAI704I-REST/Media/background.jpg"));
 		background.setBounds(0, 0, 1137, 709);
 		contentPane.add(background);
 	}
